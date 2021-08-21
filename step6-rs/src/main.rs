6dc2f64302c80d5cc7ea5ab69b4c73743d240c33
superlou <lousimons@gmail.com>
2021-08-20 21:16:24 -0400

    Extracted actions

--- step6-rs/src/main.rs
+++ step6-rs/src/main.rs
@@ -1,25 +1,12 @@
 mod a_star;
+mod actions;
 
 use a_star::{a_star, Neighbor};
+use actions::{move_actor, open_door, traverse_door, Action, DoorState};
 
 type PosId = usize;
 type DoorId = usize;
 type PosMoveGroupId = usize;
-
-#[derive(Debug, Clone)]
-pub enum Action {
-    Move { to: PosId },
-    OpenDoor { door: DoorId },
-    TraverseDoor { door: DoorId },
-}
-
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
-pub enum DoorState {
-    Open,
-    Closed,
-    Locked,
-    Broken,
-}
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct State {
@@ -33,103 +20,47 @@ pub struct World {
     pub door_side_b: Vec<DoorId>,
 }
 
-fn heuristic(state: &State) -> f32 {
-    let mut distance = 0.0;
-
-    if state.actor_pos != 5 {
-        distance += 1.0;
-    }
-
-    if state.door_states[0] != DoorState::Open {
-        distance += 1.0;
-    }
-
-    distance
-}
-
-fn neighbors(state: &State, world: &World) -> Vec<Neighbor<State, Action>> {
+fn get_neighbors(state: &State, world: &World) -> Vec<Neighbor<State, Action>> {
     let mut actions = vec![];
 
-    for i in 0..8 {
+    for i in 0..world.pos_move_groups.len() {
         actions.push(move_actor(state, world, i));
     }
 
-    for i in 0..1 {
+    for i in 0..state.door_states.len() {
         actions.push(open_door(state, world, i));
     }
 
-    for i in 0..1 {
+    for i in 0..state.door_states.len() {
         actions.push(traverse_door(state, world, i))
     }
 
     actions.into_iter().flatten().collect()
 }
 
-fn move_actor(state: &State, world: &World, to: PosId) -> Option<Neighbor<State, Action>> {
-    let precondition = (state.actor_pos != to)
-        && (world.pos_move_groups[state.actor_pos] == world.pos_move_groups[to]);
+fn heuristic(state: &State) -> f32 {
+    let mut distance = 0.0;
 
-    if !precondition {
-        return None;
+    if state.actor_pos != 9 {
+        distance += 1.0;
     }
 
-    let mut new_state = state.clone();
-    new_state.actor_pos = to;
-
-    Some(Neighbor::new(new_state, 1.0, Action::Move { to }))
-}
-
-fn open_door(state: &State, world: &World, door: DoorId) -> Option<Neighbor<State, Action>> {
-    let precondition = ((state.actor_pos == world.door_side_a[door])
-        || (state.actor_pos == world.door_side_a[door]))
-        && state.door_states[door] == DoorState::Closed;
-
-    if !precondition {
-        return None;
-    }
-
-    let mut new_state = state.clone();
-    new_state.door_states[door] = DoorState::Open;
-
-    Some(Neighbor::new(new_state, 1.0, Action::OpenDoor { door }))
-}
-
-fn traverse_door(state: &State, world: &World, door: DoorId) -> Option<Neighbor<State, Action>> {
-    let actor_pos = state.actor_pos;
-    let door_side_a = world.door_side_a[door];
-    let door_side_b = world.door_side_b[door];
-
-    let precondition = (actor_pos == door_side_a || actor_pos == door_side_b)
-        && state.door_states[door] == DoorState::Open;
-
-    if !precondition {
-        return None;
-    }
-
-    let new_pos = if actor_pos == door_side_a {
-        door_side_b
-    } else {
-        door_side_a
-    };
-
-    let mut new_state = state.clone();
-    new_state.actor_pos = new_pos;
-    Some(Neighbor::new(new_state, 1.0, Action::TraverseDoor { door }))
+    distance
 }
 
 fn main() {
     let s0 = State {
         actor_pos: 0,
-        door_states: vec![DoorState::Closed],
+        door_states: vec![DoorState::Closed, DoorState::Closed],
     };
 
     let w = World {
-        pos_move_groups: vec![1, 1, 1, 1, 2, 2, 2, 2],
-        door_side_a: vec![3],
-        door_side_b: vec![4],
+        pos_move_groups: vec![1, 1, 1, 1, 2, 2, 2, 2, 3, 3],
+        door_side_a: vec![3, 7],
+        door_side_b: vec![4, 8],
     };
 
-    let result = a_star(&s0, &heuristic, &|s: &State| neighbors(s, &w));
+    let result = a_star(&s0, &heuristic, &|s: &State| get_neighbors(s, &w));
 
     match result {
         Ok(path) => {
